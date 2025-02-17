8469ee379a7e7945159a6c17346fbb8a1face33d
Brentebeer <Brent.Dardenne@hotmail.be>
2025-02-17 22:24:03 +0100

    Create new project class library with abstraction on MediatR for CQRS command and query separation

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+//Empty ICommand that doesn't return any response
+public interface ICommand : ICommand<Unit>
+{
+
+}
+
+//ICommand that does return a respone --> IRequest<TResponse>
+//ICommand<out TResponse> = Wilt zeggen dat alle children van TRespone hier aan kunnen worden mee gegeven.
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+
+}
---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+//IQuery that does return a respone --> IRequest<TResponse>
+//IQuery<out TResponse> = Wilt zeggen dat alle children van TRespone hier aan kunnen worden mee gegeven.
+//Where TResponse : notnull = Wilt zeggen dat je geen null waarde kan toewijzen als de response niet null kan zijn.
+public interface IQuery<out TResponse> : IRequest<TResponse> 
+    where TResponse : notnull
+{
+}
