31d929c7b2f332fe48094be7ee802738c558cfed
Brentebeer <Brent.Dardenne@hotmail.be>
2025-02-18 22:52:34 +0100

    Finishing abstraction on mediatR for CQRS command and query separation

--- src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -8,8 +8,9 @@ public interface ICommand : ICommand<Unit>
 
 }
 
+//In wilt echt zeggen dat specifiek type en out wil zeggen eender welk type als het maar afgeleid is van bijvoorbeeld TResponse
 //ICommand that does return a respone --> IRequest<TResponse>
-//ICommand<out TResponse> = Wilt zeggen dat alle children van TRespone hier aan kunnen worden mee gegeven.
+//ICommand<out TResponse> = Wilt zeggen dat alle children van TRespone hier aan kunnen worden mee gegeven. (bijvoorbeeld als TResponse een basisklasse is, kun je een afgeleide klasse als type gebruiken).
 public interface ICommand<out TResponse> : IRequest<TResponse>
 {
 
---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface ICommandHandler<in TCommand> : ICommandHandler<TCommand, Unit> 
+    where TCommand : ICommand<Unit>;
+
+//In wilt echt zeggen dat specifiek type en out wil zeggen eender welk type als het maar afgeleid is van bijvoorbeeld TResponse
+//in: wilt zeggen dat het een object dat ICommand<TResponse> moet bevatten
+//where TResponse: notnull: Dit plaatst een beperking op de generieke typeparameter TResponse.Het betekent dat TResponse niet null mag zijn.
+//where TCommand: ICommand<TResponse>: Dit plaatst een beperking op de generieke typeparameter TCommand. Het betekent dat TCommand een type moet zijn dat ICommand<TResponse> implementeert.
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse> 
+    where TCommand : ICommand<TResponse> 
+    where TResponse : notnull
+{
+}
