007afacf9a883cadf8df4f138e33999b5e8e9d0f
Brentebeer <Brent.Dardenne@hotmail.be>
2025-03-12 22:32:48 +0100

    Installing martin and implementing IDocumentSession orm (Dbcontext) but for postgres

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,13 +1,10 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string name, List<string> category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid id);
 
     //Application logic layer
-    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
@@ -23,10 +20,11 @@ namespace Catalog.API.Products.CreateProduct
             };
 
             //Save to database
-
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
 
             //return createProdcutResult result
-            return new CreateProductResult(Guid.NewGuid());
+            return new CreateProductResult(product.Id);
         }
     }
 }
