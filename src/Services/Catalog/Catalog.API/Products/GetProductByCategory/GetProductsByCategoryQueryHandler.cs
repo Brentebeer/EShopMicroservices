dbe8f453d275b56071eee0fe5f9b20f761b34c56
Brentebeer <Brent.Dardenne@hotmail.be>
2025-06-01 12:33:10 +0200

    New implementation that allows to search product on category

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductsByCategoryQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductsByCategoryQuery(string Category) : IQuery<GetProductsByCategoryResult>;
+    public record GetProductsByCategoryResult(IEnumerable<Product> Products);
+    internal class GetProductsByCategoryQueryHandler(
+        IDocumentSession session,
+        ILogger<GetProductsByCategoryQueryHandler> logger) 
+        : IQueryhandler<GetProductsByCategoryQuery, GetProductsByCategoryResult>
+    {
+        public async Task<GetProductsByCategoryResult> Handle(GetProductsByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByCategoryQueryHandler.Handle Called with {@query}", query);
+
+            var products = await session.Query<Product>().Where(p => p.Category.Contains(query.Category)).ToListAsync();
+
+            return new GetProductsByCategoryResult(products);
+        }
+    }
+}
