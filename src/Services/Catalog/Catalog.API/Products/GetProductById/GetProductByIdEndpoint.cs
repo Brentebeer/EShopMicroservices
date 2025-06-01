3dd19c00c42354cc2ae0ccaa462685dbce0cfa56
Brentebeer <Brent.Dardenne@hotmail.be>
2025-06-01 12:32:33 +0200

    fixing spelling mistake

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -18,7 +18,7 @@
             .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
             .ProducesProblem(StatusCodes.Status400BadRequest)
             .WithSummary("Get Product By Id")
-            .WithDescription("Get Prodcut By Id");
+            .WithDescription("Get Product By Id");
         }
     }
 }
