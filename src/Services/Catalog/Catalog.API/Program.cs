7ee99633024b912f1933e3cda8202ae191f67c09
Brentebeer <Brent.Dardenne@hotmail.be>
2025-07-26 15:34:34 +0200

    General exception hanlder whatever isn't handled locally with trye catch end up here makes also that the exeception is in json formatting to give a more clear and easier the read look

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,6 @@
 using BuildingBlocks.Behaviors;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,5 +33,33 @@ var app = builder.Build();
 
 // Configure to HTTP request pipeline
 app.MapCarter();
+
+//It configures a global exception handler for your application. This means that if an error occurs somewhere in your app that isn't handled locally (with try/catch), it will end up here.
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if(exception == null)
+        {
+            return;
+        }
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, exception.Message);
+
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+
+        await context.Response.WriteAsJsonAsync(problemDetails);
+    });
+});
 
 app.Run();
