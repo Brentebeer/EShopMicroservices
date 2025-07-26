d3127aee1cca0ab05d210bd453cd96fd405367c6
Brentebeer <Brent.Dardenne@hotmail.be>
2025-07-26 15:33:05 +0200

    Fluent validation for Update and Delete Product command handlers

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandHandler.cs
@@ -1,10 +1,20 @@
 ﻿
 using Catalog.API.Models;
+using Catalog.API.Products.UpdateProduct;
 
 namespace Catalog.API.Products.DeleteProduct
 {
     public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
     public record DeleteProductResult(bool IsSuccess);
+
+    public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+    {
+        public DeleteProductCommandValidator()
+        {
+            RuleFor(command => command.Id).NotEmpty().WithMessage("Product ID is required");
+        }
+    }
+
     internal class DeleteProductCommandHandler (
         IDocumentSession session,
         ILogger<DeleteProductCommandHandler> logger
