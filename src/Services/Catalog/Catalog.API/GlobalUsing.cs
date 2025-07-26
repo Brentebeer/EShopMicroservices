7f2c9eb147e3f329ffb72940da64d9d843a3dbf9
Brentebeer <Brent.Dardenne@hotmail.be>
2025-07-26 17:10:33 +0200

    CustomExceptionHandler implementation

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -1,8 +1,10 @@
 ﻿global using Carter;
 global using Mapster;
 global using MediatR;
-global using BuildingBlocks.CQRS;
-global using Catalog.API.Models;
-global using Catalog.API.Exceptions;
 global using Marten;
 global using FluentValidation;
+global using Catalog.API.Models;
+global using Catalog.API.Exceptions;
+global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions.Handler;
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,12 +1,6 @@
-using BuildingBlocks.Behaviors;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
-
-
 var assembly = typeof(Program).Assembly;
 
 //Underneath is pipelinebehavior
@@ -29,37 +23,13 @@ builder.Services.AddMarten(opts =>
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
 
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 var app = builder.Build();
 
 // Configure to HTTP request pipeline
 app.MapCarter();
 
-//It configures a global exception handler for your application. This means that if an error occurs somewhere in your app that isn't handled locally (with try/catch), it will end up here.
-app.UseExceptionHandler(exceptionHandlerApp =>
-{
-    exceptionHandlerApp.Run(async context =>
-    {
-        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
-        if(exception == null)
-        {
-            return;
-        }
-
-        var problemDetails = new ProblemDetails
-        {
-            Title = exception.Message,
-            Status = StatusCodes.Status500InternalServerError,
-            Detail = exception.StackTrace
-        };
-
-        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
-        logger.LogError(exception, exception.Message);
-
-        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-        context.Response.ContentType = "application/problem+json";
-
-        await context.Response.WriteAsJsonAsync(problemDetails);
-    });
-});
+app.UseExceptionHandler(options => { });
 
 app.Run();
