704fc40f76e3038ae6f19fb47caab80570828ae8
Brentebeer <Brent.Dardenne@hotmail.be>
2025-07-26 14:57:44 +0200

    Refactoring CreateProductHandler to use pipeline behavior and fluent validation

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -16,20 +16,14 @@
 
     //Application logic layer
     internal class CreateProductCommandHandler(
-        IDocumentSession session,
-        IValidator<CreateProductCommand> validator
+        IDocumentSession session, ILogger<CreateProductCommandHandler> logger
         ) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
             //Create product entity from command object.
 
-            var result = await validator.ValidateAsync(command, cancellationToken);
-            var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
-            if (errors.Any())
-            {
-                throw new ValidationException(errors.FirstOrDefault());
-            }
+            logger.LogInformation("CreateProductCommandHandler.Handle called with {@command}", command);
 
             var product = new Product
             {
